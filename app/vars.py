536fc2a4c9adc5119f60dd0457a79cbca9670bff
Mixail132 <millionbaxov@gmail.com>
2024-07-31 22:12:59 +0300

    Fix errors when reading the ini file.

--- app/vars.py
+++ app/vars.py
@@ -13,11 +13,11 @@ class BotVars:
         config_sections.read("vars.ini", "utf-8")
         parser = config_sections["VARS"].parser
         self.bot_configs = {
-            par_name: par_value
+            par_name.upper(): par_value
             for par_name, par_value
             in parser.section["BOT_CONFIGS"].items()
         }
         self.bot_users = {
-            user_name: user_id
+            user_name.upper(): user_id
             for user_name, user_id
             in parser.section["BOT_USERS"].items()}
