3dbb8f486ee210cb05513930bd62314e2872b7d8
Mixail132 <millionbaxov@gmail.com>
2024-07-31 16:35:53 +0300

    Extract and collect the bot variables.

---
+++ app/vars.py
@@ -0,0 +1,23 @@
+import configparser
+
+
+class IniSection(configparser.ConfigParser):
+    @property
+    def section(self):
+        return self._sections
+
+
+class BotVars:
+    def __init__(self):
+        config_sections = IniSection()
+        config_sections.read("vars.ini", "utf-8")
+        parser = config_sections["VARS"].parser
+        self.bot_configs = {
+            par_name: par_value
+            for par_name, par_value
+            in parser.section["BOT_CONFIGS"].items()
+        }
+        self.bot_users = {
+            user_name: user_id
+            for user_name, user_id
+            in parser.section["BOT_USERS"].items()}
