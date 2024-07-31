81eccdc2b5795cebddc38c8fc3726c4891478146
Mixail132 <millionbaxov@gmail.com>
2024-07-31 18:20:03 +0300

    Add a Flask app and the bot message handler.

---
+++ app/bot.py
@@ -0,0 +1,23 @@
+from config import viber
+from viberbot.api.viber_requests import (
+    ViberMessageRequest,
+    ViberConversationStartedRequest)
+from flask import Flask, request, Response
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def incoming():
+    viber_request = viber.parse_request(request.get_data())
+    if isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message
+        viber.send_messages(viber_request.sender.id, [message])
+    elif isinstance(viber_request, ViberConversationStartedRequest):
+        print(viber_request.user.id)
+    return Response(status=200)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8443, debug=True)
