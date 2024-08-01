822c260ba42a0365787808e5ed027f9381b0792f
Mixail132 <millionbaxov@gmail.com>
2024-08-01 14:29:15 +0300

    Remove temporary endpoint for checking.

--- app/bot.py
+++ app/bot.py
@@ -19,10 +19,5 @@ def incoming():
     return Response(status=200)
 
 
-@app.route("/")
-def checking():
-    return "Hello, World!"
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8443, debug=True)
