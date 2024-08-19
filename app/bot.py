7c81eed53a588c0d4c56d75f1eb094791d18f452
Mixail132 <millionbaxov@gmail.com>
2024-08-19 20:43:25 +0300

    Fix pylint note.

--- app/bot.py
+++ app/bot.py
@@ -1,4 +1,5 @@
 """ The Flask application to handle incoming requests to the bot. """
+
 from config import viber, bot_vars
 from viberbot.api.viber_requests import ViberMessageRequest
 from viberbot.api.viber_requests import ViberConversationStartedRequest
