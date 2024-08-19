d05df994fc52860e270d69ff0f2760579d288be8
Mixail132 <millionbaxov@gmail.com>
2024-08-19 20:28:26 +0300

    Set a single function to a single action in the class.

--- app/vars.py
+++ app/vars.py
@@ -14,16 +14,22 @@ class IniSection(configparser.ConfigParser):
 
 
 class BotVars:
-    """ Reads the vars from the configuration file. """
-    ini_root = Path(__file__).parent.resolve()
-    ini_file = ini_root / "vars.ini"
+    """ Reads and sets the variables from the configuration file. """
+
+    @staticmethod
+    def read_config():
+        """ Reads the variables from the configuration file. """
+        ini_root = Path(__file__).parent.resolve()
+        ini_file = ini_root / "vars.ini"
+        config_sections = IniSection()
+        config_sections.read(ini_file, "utf-8")
+        parser = config_sections["VARS"].parser
+
+        return parser
 
     def __init__(self):
         """ Sets up the environment variables once creating an instance."""
-        config_sections = IniSection()
-        config_sections.read(self.ini_file, "utf-8")
-        parser = config_sections["VARS"].parser
-
+        parser = self.read_config()
         self.configs = dict(parser.section["BOT_CONFIGS"].items())
         self.users = dict(parser.section["BOT_USERS"].items())
         self.admin = parser.section["BOT_USERS"]["admin"]
